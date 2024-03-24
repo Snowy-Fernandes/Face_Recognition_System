e7f98134a5fb7d179ceb1c4f7b61acb75fa36058
Snowy Fernandes <157784566+Snowy-Fernandes@users.noreply.github.com>
2024-03-24 22:29:27 +0530

    Update attendanceProject.py

--- attendanceProject.py
+++ attendanceProject.py
@@ -4,8 +4,7 @@ import numpy as np
 import os
 import csv
 from datetime import datetime
-# from PIL import ImageGrab
- 
+
 path = 'Photos'
 images = []
 classNames = []
@@ -45,13 +44,7 @@ def markAttendance(name):
             if name not in nameList:
                 dtString = now.strftime('%H:%M:%S')
                 writer.writerow([name, dtString])
-           
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-# def captureScreen(bbox=(300,300,690+300,530+300)):
-#     capScr = np.array(ImageGrab.grab(bbox))
-#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-#     return capScr
- 
+
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
  
@@ -59,7 +52,6 @@ cap = cv2.VideoCapture(0)
  
 while True:
     success, img = cap.read()
-    #img = captureScreen()
     imgS = cv2.resize(img,(0,0),None,0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
  
@@ -69,12 +61,10 @@ while True:
     for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
-        #print(faceDis)
         matchIndex = np.argmin(faceDis)
  
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
-            #print(name)
             y1,x2,y2,x1 = faceLoc
             y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
